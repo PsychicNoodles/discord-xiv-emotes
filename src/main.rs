f24ff173f6bd41f4222bfc89432689e1bb89895a
Mattori Birnbaum <mattori.birnbaum@gmail.com>
2022-10-14 05:39:07 +0000

    replaced stderr prints with logging

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use dotenv::dotenv;
+use log::*;
 use std::env;
 use thiserror::Error;
 
@@ -51,6 +52,7 @@ async fn check_other_cmd(
 ) -> Result<bool, HandlerError> {
     match mparts[..] {
         [_cmd] if _cmd == "emotes" => {
+            trace!("emotes command");
             let emote_list: Vec<_> = log_message_repo.emote_list().cloned().collect();
             msg.reply(
                 context,
@@ -60,6 +62,7 @@ async fn check_other_cmd(
             Ok(true)
         }
         [_cmd] if _cmd == "help" => {
+            trace!("help command");
             msg.reply(context, format!("Use emotes from FFXIV in chat, optionally with a (mentionable) target! Use {}emotes for a list of options.", PREFIX)).await?;
             Ok(true)
         }
@@ -74,6 +77,7 @@ async fn process_input(
     msg: &Message,
 ) -> Result<Option<(ConditionTexts, LogMessageAnswers, String)>, HandlerError> {
     if check_other_cmd(mparts, log_message_repo, context, msg).await? {
+        debug!("non-emote command");
         return Ok(None);
     }
 
@@ -82,9 +86,11 @@ async fn process_input(
         [e] => Ok((["/", e].concat(), None)),
         _ => Err(HandlerError::InvalidFormat(mparts.join(" "))),
     }?;
+    trace!("parsed command and mention: {:?} {:?}", cmd, mention);
 
     match (&cmd, mention) {
         (emote, Some(mention)) if log_message_repo.contains_emote(emote) => {
+            debug!("emote with mention");
             let messages = log_message_repo.messages(emote)?;
             // todo allow setting gender
             let origin = Character::new_from_string(
@@ -95,16 +101,19 @@ async fn process_input(
                 true,
                 false,
             );
+            trace!("message origin: {:?}", origin);
             let target_name = match determine_mention(&msg, &context).await {
                 Some(n) => n,
                 None => mention.to_string(),
             };
             let target = Character::new_from_string(target_name.clone(), Gender::Male, true, false);
+            trace!("message target: {:?}", target);
             let answers = LogMessageAnswers::new(origin, target)?;
             let condition_texts = extract_condition_texts(&messages.en.targeted)?;
             Ok(Some((condition_texts, answers, target_name)))
         }
         (emote, None) if log_message_repo.contains_emote(emote) => {
+            debug!("emote without mention");
             let messages = log_message_repo.messages(emote)?;
             // todo allow setting gender
             let origin = Character::new_from_string(
@@ -115,6 +124,7 @@ async fn process_input(
                 true,
                 false,
             );
+            trace!("message origin: {:?}", origin);
             let answers = LogMessageAnswers::new(origin, UNTARGETED_TARGET)?;
             let condition_texts = extract_condition_texts(&messages.en.untargeted)?;
             Ok(Some((
@@ -129,10 +139,12 @@ async fn process_input(
 
 async fn determine_mention(msg: &Message, context: &Context) -> Option<String> {
     if let Some(user) = msg.mentions.first() {
+        trace!("mention appears to be a user");
         user.nick_in(context, msg.guild_id?)
             .await
             .or(Some(user.name.clone()))
     } else if let Some(role_id) = msg.mention_roles.first() {
+        trace!("mention appears to be a role");
         let role = msg
             .guild(context.cache.clone())?
             .roles
@@ -141,8 +153,10 @@ async fn determine_mention(msg: &Message, context: &Context) -> Option<String> {
             .clone();
         Some(format!("every {} in sight", role))
     } else if msg.mention_everyone {
+        trace!("mention appears to be everyone");
         Some("everyone in the vicinity".to_string())
     } else {
+        trace!("no mention found");
         None
     }
 }
@@ -150,15 +164,17 @@ async fn determine_mention(msg: &Message, context: &Context) -> Option<String> {
 #[async_trait]
 impl EventHandler for Handler {
     async fn message(&self, context: Context, msg: Message) {
-        if msg.content.starts_with(PREFIX) {
+        trace!("incoming message: {:?}", msg);
+        if !msg.is_own(&context) && msg.content.starts_with(PREFIX) {
             let mparts: Vec<_> = msg.content[1..].split_whitespace().collect();
+            debug!("message parts: {:?}", mparts);
             if let Some((condition_texts, answers, target_name)) =
                 match process_input(&mparts, &self.log_message_repo, &context, &msg).await {
                     Ok(v) => v,
                     Err(err) => {
-                        eprintln!("error during processing: {:?}", err);
+                        error!("error during processing: {:?}", err);
                         if let Err(e) = msg.reply(context, err.to_string()).await {
-                            eprintln!(
+                            error!(
                                 "could not send follow-up message ({}): {:?}",
                                 err.to_string(),
                                 e
@@ -183,7 +199,7 @@ impl EventHandler for Handler {
                     };
                 });
                 if let Err(e) = msg.reply(&context, msg_builder.build()).await {
-                    eprintln!("failed to send emote message: {:?}", e);
+                    error!("failed to send emote message: {:?}", e);
                     return;
                 }
             }
@@ -193,15 +209,16 @@ impl EventHandler for Handler {
 
 #[tokio::main]
 async fn main() {
+    pretty_env_logger::init();
     dotenv().ok();
-    let token = env::var("DISCORD_TOKEN").expect("expected a discord token in the env");
+    let token = env::var("DISCORD_TOKEN").expect("expected DISCORD_TOKEN env var");
     let intents = GatewayIntents::GUILD_MESSAGES
         | GatewayIntents::DIRECT_MESSAGES
         | GatewayIntents::MESSAGE_CONTENT;
     let log_message_repo = LogMessageRepository::from_xivapi(None)
         .await
         .expect("couldn't load log message data from xivapi");
-    println!(
+    info!(
         "repo initialized with emotes: {:?}",
         log_message_repo.emote_list().collect::<Vec<_>>()
     );
@@ -210,7 +227,5 @@ async fn main() {
         .await
         .expect("error creating client");
 
-    if let Err(e) = client.start().await {
-        eprintln!("client error: {:?}", e);
-    }
+    client.start().await.expect("couldn't start client");
 }
