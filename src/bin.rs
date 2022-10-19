4118f19e2db7dab39af3b2d0bdb1f28610500b78
Mattori Birnbaum <mattori.birnbaum@gmail.com>
2022-10-19 06:49:04 +0000

    add logs for successfully connecting to db

--- src/bin.rs
+++ src/bin.rs
@@ -2,6 +2,7 @@ use std::env;
 
 use discord_xiv_emotes::setup_client;
 use dotenv::dotenv;
+use log::info;
 use sqlx::PgPool;
 
 #[tokio::main]
@@ -9,9 +10,11 @@ async fn main() {
     pretty_env_logger::init();
     dotenv().ok();
     let token = env::var("DISCORD_TOKEN").expect("expected DISCORD_TOKEN env var");
-    let pool = PgPool::connect(&env::var("DATABASE_URL").expect("expected DATABASE_URL env var"))
+    let db_url = env::var("DATABASE_URL").expect("expected DATABASE_URL env var");
+    let pool = PgPool::connect(&db_url)
         .await
         .expect("could not connect to database");
+    info!("connected to db at {}", db_url);
     let mut client = setup_client(token, pool).await;
 
     client.start().await.expect("couldn't start client");
