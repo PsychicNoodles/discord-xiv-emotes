f3d1e1dc244e1228fe219ea9aa673107a422e90d
Mattori Birnbaum <mattori.birnbaum@gmail.com>
2022-10-21 04:19:12 +0000

    add guilds table migrations

---
+++ migrations/002_create_guilds.up.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS guilds (
+	guild_id bigserial PRIMARY KEY,
+	discord_id char(20) NOT NULL UNIQUE,
+	language int NOT NULL,
+	gender int NOT NULL,
+	commands_enabled boolean NOT NULL,
+	insert_tm timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	update_tm timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
